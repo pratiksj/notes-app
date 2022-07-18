d897cce22b769b7e14c88ef9d4c20b6893cc6349
pratiksj <pratiksharai728@gmail.com>
2022-07-18 15:10:13 +0545

    change the controlled input

--- react-notes-app/src/App.js
+++ react-notes-app/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
+import Note from "./components/Note";
 
-function App() {
+const App=(props)=> {
+  const [note,setNote]=useState(props.notes)
+  const [newnote,setnewNote]=useState("pratiksha")
+  
+  const addNote=(event)=>{
+    event.preventDefault()
+  console.log(event.target)
+
+  }
+  const handleNoteChange=(event)=>{
+    console.log(event.target.value)
+    setnewNote(event.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hellow there</h1>
+      <ul>
+      {props.notes.map((x) => (
+          <Note key={x.id} note={x} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value ={newnote} onChange={handleNoteChange}/>
+        <button>click me</button>
+      </form>
+      
     </div>
   );
 }
