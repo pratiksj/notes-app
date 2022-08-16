4bf7d03fe5ded645dec48369c2ac00082a90d21a
pratiksj <pratiksharai728@gmail.com>
2022-08-16 21:41:20 +0545

    set up error message for the id that does not exist

--- part-3/notes-server/index.js
+++ part-3/notes-server/index.js
@@ -33,6 +33,7 @@ App.get("/notes",(request,response)=>{
 
 App.get("/notes/:id",(request,response)=>{
   const currentId = Number(request.params.id);
+  console.log(currentId)
   const thisNote = notes.find((note)=>note.id === currentId)
   if (thisNote) response.json(thisNote)
   else response.status(404).json({error:404, message:`There is no note with id ${currentId}`})
