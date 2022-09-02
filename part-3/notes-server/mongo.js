0b501230fe944b11ae0dd451d945130d3730eea3
pratiksj <pratiksharai728@gmail.com>
2022-09-02 14:57:12 +0545

    configuring url

--- part-3/notes-server/mongo.js
+++ part-3/notes-server/mongo.js
@@ -4,13 +4,13 @@ if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
+console.log(process.env.MONG) //node ma environment variable pass garna multiple  ways huncha for example command line pass garnne or .env file bata pani pas garna skincha
 
 const password = process.argv[2]
  
 //const url = `mongodb+srv://:${password}@cluster0.cnk2vze.mongodb.net/NewDatabase?retryWrites=true&w=majority`
 //const url = `mongodb+srv://:${password}@cluster0.cnk2vze.mongodb.net/NewDatabase?retryWrites=true&w=majority`
 const url =`mongodb+srv://pratiksha:${password}@cluster0.cnk2vze.mongodb.net/MyNotes?retryWrites=true&w=majority`
-
 
 const noteSchema = new mongoose.Schema({
 
@@ -33,10 +33,10 @@ mongoose
     // })
     // return note.save()
 
-const notes= Note.find({important:false})
-return notes
+const notes= Note.find({important:false}) //database call lai nai asynchronous call vanincha ani  tsma promise feri fakincha
+return notes//asynchronous call garepachi generally javascript ma promise return garxa 
   })
-  .then((result) => {
+  .then((result) => { // ani farkeko promise resolve hunasath garnu parne action .then action ma hunxa 
     result.forEach(note => {
         console.log(note)
       })
