a021988876c136393559f0f4ef9bb4176ee2ee4a
pratiksj <pratiksharai728@gmail.com>
2022-10-29 22:00:10 +0545

    configured the eslint

--- react-notes-app/src/components/Togglable.js
+++ react-notes-app/src/components/Togglable.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, forwardRef, useImperativeHandle } from "react";
 import PropTypes from "prop-types";
-
-const Togglable = (props) => {
+// eslint-disable-next-line react/display-name
+const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false);
 
   const hideWhenVisible = { display: visible ? "none" : "" };
@@ -10,6 +10,12 @@ const Togglable = (props) => {
   const toggleVisibility = () => {
     setVisible(!visible);
   };
+
+  useImperativeHandle(refs, () => {
+    return {
+      toggleVisibility,
+    };
+  });
 
   return (
     <div>
@@ -22,9 +28,11 @@ const Togglable = (props) => {
       </div>
     </div>
   );
-};
+});
 Togglable.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
 };
+
+Togglable.displayName = "Togglable";
 
 export default Togglable;
