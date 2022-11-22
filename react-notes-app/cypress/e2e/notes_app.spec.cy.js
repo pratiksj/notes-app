c0c3361142ac8221530a35cbdc40328226792fbb
pratiksj <pratiksharai728@gmail.com>
2022-11-22 12:51:28 +0545

    feat: controlling the state of the database

--- react-notes-app/cypress/e2e/notes_app.spec.cy.js
+++ react-notes-app/cypress/e2e/notes_app.spec.cy.js
@@ -1,7 +1,15 @@
 describe('Note app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'john',
+      username: 'john',
+      password: 'radar'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user) 
     cy.visit('http://localhost:3000')
   })
+  
 
   it('front page can be opened', function() {
     cy.contains('Notes')
@@ -19,5 +27,38 @@ describe('Note app', function() {
     cy.get('#login-button').click()
 
     cy.contains('john logged-in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('login').click()
+      cy.get('input:first').type('john')
+      cy.get('input:last').type('radar')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('create a new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+    describe('and a note exists', function () {
+      beforeEach(function () {
+        cy.contains('create a new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+      it('it can be made important', function () {
+        cy.contains('another note cypress')
+          .contains('make important')
+          .click()
+
+        cy.contains('another note cypress')
+          .contains('make not important')
+      })
+    })
+
   })
   })
